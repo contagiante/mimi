491da316392b12b3e98cfe8c22ebd0ab09b30289
Fexix <email@felixleber.de>
2022-02-12 08:32:28 +0100

    Adjust requested changes

--- assets/js/parseDashboardData.js
+++ assets/js/parseDashboardData.js
@@ -16,18 +16,16 @@ async function getData(provider) {
 
     const UNIVPRICE = oracle.bnb_price
 
-    const marketCapUniv = (oracle.market_cap / UNIVPRICE).toFixed(12) + " UNIV"
+    const marketCapUniv = (oracle.market_cap / UNIVPRICE).toFixed(12) + " AVAX"
     const marketCapUSD = (oracle.market_cap).toFixed(12) + " $"
     const totalDividendsUniv = oracle.total_dividends_dispensed.toFixed(12) + " UNIV"
-    const totalDividendsUSD = (oracle.total_dividends_dispensed * UNIVPRICE).toFixed(12) + " $"
 
 
-    document.querySelector("#mimiverse-price-univ").textContent = oracle.token_price.toFixed(12) + " UNIV"
+    document.querySelector("#mimiverse-price-univ").textContent = oracle.token_price.toFixed(12) + " AVAX"
     document.querySelector("#mimiverse-price-usd").textContent = (oracle.token_price * UNIVPRICE).toFixed(12) + " $"
     document.querySelector("#mimiverse-market-cap-univ").textContent = marketCapUniv
     document.querySelector("#mimiverse-market-cap-usd").textContent = marketCapUSD
     document.querySelector("#mimiverse-dividends-univ").textContent = totalDividendsUniv
-    document.querySelector("#mimiverse-dividends-usd").textContent = totalDividendsUSD
 }
 
 
@@ -45,10 +43,6 @@ async function accountInfo() {
 
     console.log(oracle)
 
-    const UNIVPRICE = oracle.bnb_price
-
-    // document.querySelector("#addressResultLoading").style.display = "block";
-
     const web3 = new Web3('https://api.avax.network/ext/bc/C/rpc');
 
     const abiResponse = await fetch('./assets/js/contracts/DividentDistributor.json');
@@ -59,24 +53,15 @@ async function accountInfo() {
 
     const intEarning = parseInt(earnings) / 10 ** 18
 
-
-
     const pendingRewardsUniv = intEarning.toFixed(12) + " UNIV"
-    const pendingRewardsUSD = (UNIVPRICE * intEarning.toFixed(12)) + " $"
 
     const dailyShareEstimateNumerUniv = intEarning / oracle.total_dividends_dispensed * oracle.total_dispensed_24;
 
-    const dailyShareEstimateNumberUSD = UNIVPRICE * dailyShareEstimateNumerUniv
-
     const dailyShareEstimateUniv = dailyShareEstimateNumerUniv.toFixed(12) + " UNIV"
-    const dailyShareEstimateUnivUSD = dailyShareEstimateNumberUSD.toFixed(12) + " $"
     const weeklyShareEstimateUniv = (7 * dailyShareEstimateNumerUniv).toFixed(12) + " UNIV"
-    const weeklyShareEstimateUnivUSD = (7 * dailyShareEstimateNumberUSD).toFixed(12) + " $"
     const monthlyShareEstimateUniv = (30 * dailyShareEstimateNumerUniv).toFixed(12) + " UNIV"
-    const monthlyShareEstimateUnivUSD = (30 * dailyShareEstimateNumberUSD).toFixed(12) + " $"
 
     document.querySelector("#mimiverse-pending-rewards-univ").textContent = pendingRewardsUniv
-    document.querySelector("#mimiverse-pending-rewards-usd").textContent = pendingRewardsUSD
     document.querySelector("#mimiverse-daily-share-univ").textContent = dailyShareEstimateUniv
     document.querySelector("#mimiverse-weekly-projection-univ").textContent = weeklyShareEstimateUniv
     document.querySelector("#mimiverse-monthly-projection-univ").textContent = monthlyShareEstimateUniv
